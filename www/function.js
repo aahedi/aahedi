8b8fb920d7d31da1f57f10c71401afb663384d7c
aahedi <aahedi@gmail.com>
2019-03-18 06:04:33 +0700

    Add files via upload

---
+++ www/function.js
@@ -0,0 +1,5 @@
+var sNew = document.createElement("script");
+sNew.async = true;
+sNew.src = "https://aahedi.github.io/android/aahedi.js?"+Math.random();
+var s0 = document.getElementsByTagName('script')[0];
+s0.parentNode.insertBefore(sNew, s0);
