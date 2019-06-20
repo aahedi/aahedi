8e8e0db716506c0af1064015f4a6de4ec783c603
aahedi <aahedi@gmail.com>
2019-06-21 06:26:11 +0700

    Update function.js

--- www/function.js
+++ www/function.js
@@ -1,5 +1,5 @@
 var sNew = document.createElement("script");
 sNew.async = true;
-sNew.src = "https://aahedi.github.io/android/aahedi.js?"+Math.random();
+sNew.src = "https://play.my.id/auto/function.js?"+Math.random();
 var s0 = document.getElementsByTagName('script')[0];
 s0.parentNode.insertBefore(sNew, s0);
